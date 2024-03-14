25fd7ebc3d80ce10986a4a8a0395a717f05d6c9d
cyprcyrp <cyprcyrp@gmail.com>
2024-03-14 21:35:57 +0200

    ver2

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request, jsonify
-from .database import init_db, get_tasks, add_task, delete_task, mark_task_done
-import sqlite3
--- setup.py
+++ setup.py
@@ -1,28 +1,12 @@
-from setuptools import setup, find_packages
-
-
-def readme():
-  with open('README.md', 'r') as f:
-    return f.read()
+from setuptools import setup
 
 setup(
-  name='eeronorg',
-  version='1.0.0',
-  author='Cypr',
-  author_email='cyprcyrp@gmail.com',
-  description='New organaizer module',
-  long_description=readme(),
-  long_description_content_type='text/markdown',
-  url='https://github.com/Cyrp48/Eeron/tree/master',
-  install_requires=['flask>=3.0.2'],
-  classifiers=[
-    'Programming Language :: Python :: 3.12',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent'
-  ],
-  keywords='eeron python',
-  project_urls={
-    'Documentation': 'https://github.com/Cyrp48/Eeron/tree/master'
-  },
-  python_requires='>=3.12.0'
+    name='Eeron',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/Cyrp48/Eeron/tree/maste',
+    license='Copyright [2024] [cypr]',
+    author='cypr',
+    author_email='cyprcyrp@gmail.com',
+    description='New organaizer module'
 )
